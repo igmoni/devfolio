8743f2e7c5ca618befe0dc6fb1ea434802dc4b7a
mon <mohansp119@gmail.com>
2026-01-16 21:29:38 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,10 +20,49 @@ const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800", "900"],
 });
 
+export const metadata = {
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_URL || "https://monxdev.vercel.app"
+  ),
+  title: {
+    default: "Mohan S P - Full Stack Web Developer • UI/UX Designer",
+    template: "%s | Mohan S P",
+  },
 
-export async function generateMetadata() {
-  return getMetaData("/");
-}
+  description:
+    "I design and build engaging digital experiences focused on performance and visual design, specialized in creating landing pages and portfolio websites. Explore my projects, experience & technical expertise.",
+
+  openGraph: {
+    type: "website",
+    url: "https://monxdev.vercel.app/",
+    title: "Mohan S P - Full Stack Web Developer • UI/UX Designer",
+    description:
+      "I design and build engaging digital experiences focused on performance and visual design, specialized in creating landing pages and portfolio websites. Explore my projects, experience & technical expertise.",
+    siteName: "Mohan S P",
+    images: [
+      {
+        url: "https://monxdev.vercel.app/meta/opengraph-img.png",
+        width: 1200,
+        height: 630,
+        alt: "Mohan S P Portfolio Preview",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Mohan S P - Full Stack Web Developer • UI/UX Designer",
+    description:
+      "I design and build engaging digital experiences focused on performance and visual design, specialized in creating landing pages and portfolio websites.",
+    images: ["https://monxdev.vercel.app/meta/opengraph-img.png"],
+    creator: "@_igmoni",
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 
 
 export default function RootLayout({ children }) {
