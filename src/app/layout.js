60da50a85453ae61c3b291f639959d2c8ff2af3b
mon <mohansp119@gmail.com>
2026-07-04 13:50:06 +0530

    Update project - 4

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Poppins } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Toaster } from "sonner";
-
+  
 import UmamiAnalytics from "@/components/analytics/UmamiAnalytics";
 import Footer from "@/components/common/Footer";
 import KeyboardShortcutsLayer from "@/components/common/KeyboardShortcutsLayer";
